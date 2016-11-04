409513de8a0dc60a944e8fb55d79a8a865b4af39
Andreas Pape <andreas@ioswarm.de>
2016-11-04 19:55:38 +0100

    extend Loggable

--- src/main/java/ioswarm/vertx/log/Loggable.java
+++ src/main/java/ioswarm/vertx/log/Loggable.java
@@ -7,4 +7,36 @@ public interface Loggable {
 
 	default Logger logger() { return LoggerFactory.getLogger(this.getClass()); }
 	
+	default void info(Object message) {
+		if (logger().isInfoEnabled()) logger().info(message);
+	}
+	
+	default void info(Object message, Throwable t) {
+		if (logger().isInfoEnabled()) logger().info(message, t);
+	}
+	
+	default void debug(Object message) {
+		if (logger().isDebugEnabled()) logger().debug(message);
+	}
+	
+	default void debug(Object message, Throwable t) {
+		if (logger().isDebugEnabled()) logger().debug(message, t);
+	}
+	
+	default void warn(Object message) {
+		logger().warn(message);
+	}
+	
+	default void warn(Object message, Throwable t) {
+		logger().warn(message, t);
+	}
+	
+	default void error(Object message) {
+		logger().error(message);
+	}
+	
+	default void error(Object message, Throwable t) {
+		logger().error(message, t);
+	}
+	
 }
